e83ff02db7b609001495cc27207f4fe4fbdc998c
aoi apache <aoi.182@live.com>
2020-03-29 14:39:48 -0500

    Added secod project for EF - code first approach

---
+++ Solution/WebAppCodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppCodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
